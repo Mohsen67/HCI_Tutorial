b978e6fa8329224e0aa915544747fc80e28e18ee
Mohsen67 <mohsen.r.moghadam@gmail.com>
2014-12-26 19:27:41 +0100

    generat and print a random value

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,12 @@
 #include "iostream"
+#include <stdlib.h>     /* srand, rand */
+
 //main program starts from here
 int main(){
 	std::cout << "Hello World" << std::endl;
+	int Random_Number;
+	Random_Number = rand() % 1000 + 1;
+	std::cout << "Random value generated is : " << Random_Nember << std:endl;
 	system("Pause");
 	return 0;
 }
